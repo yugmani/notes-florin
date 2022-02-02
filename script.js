60163b4ff1b88b63146f89f29f7406bb99b43269
Himalaya <yugmani@yahoo.com>
2022-02-01 23:23:00 -0800

    mark down note:single

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("Notes App");
+const notesEl = document.querySelector(".notes");
+const editBtn = document.querySelector(".edit");
+const deleteBtn = document.querySelector(".delete");
+
+const main = notesEl.querySelector(".main");
+const textArea = notesEl.querySelector("textarea");
+
+editBtn.addEventListener("click", () => {
+  main.classList.toggle("hidden");
+  textArea.classList.toggle("hidden");
+});
+
+textArea.addEventListener("input", (e) => {
+  e.preventDefault();
+  const { value } = e.target;
+  console.log(marked.parse(value));
+  main.innerHTML = marked.parse(value);
+});
