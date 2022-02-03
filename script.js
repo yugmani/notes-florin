0cdf60e54b2287b782fab24e28b98e77311cd79e
Himalaya <yugmani@yahoo.com>
2022-02-02 23:54:32 -0800

    add note:multiple notes

--- script.js
+++ script.js
@@ -1,18 +1,47 @@
-const notesEl = document.querySelector(".notes");
-const editBtn = document.querySelector(".edit");
-const deleteBtn = document.querySelector(".delete");
+// const notesEl = document.querySelector(".note");
+const addBtn = document.getElementById("add-note");
 
-const main = notesEl.querySelector(".main");
-const textArea = notesEl.querySelector("textarea");
-
-editBtn.addEventListener("click", () => {
-  main.classList.toggle("hidden");
-  textArea.classList.toggle("hidden");
+addBtn.addEventListener("click", () => {
+  addNewNote();
 });
 
-textArea.addEventListener("input", (e) => {
-  e.preventDefault();
-  const { value } = e.target;
-  console.log(marked.parse(value));
-  main.innerHTML = marked.parse(value);
-});
+function addNewNote() {
+  const note = document.createElement("div");
+  note.classList.add("note");
+
+  note.innerHTML = `
+    <div class="notes">
+      <div class="tools">
+        <button class="edit"><i class="fas fa-edit"></i></button>
+        <button class="delete"><i class="fas fa-trash-alt"></i></button>
+      </div>
+      <div class="note-area">
+         <textarea></textarea>
+         <div class="main hidden"></div>
+      </div>
+    </div>
+  `;
+
+  const editBtn = note.querySelector(".edit");
+  const deleteBtn = note.querySelector(".delete");
+
+  const main = note.querySelector(".main");
+  const textArea = note.querySelector("textarea");
+
+  deleteBtn.addEventListener("click", () => {
+    note.remove();
+  });
+
+  editBtn.addEventListener("click", () => {
+    main.classList.toggle("hidden");
+    textArea.classList.toggle("hidden");
+  });
+
+  textArea.addEventListener("input", (e) => {
+    e.preventDefault();
+    const { value } = e.target;
+    main.innerHTML = marked.parse(value);
+  });
+
+  document.body.appendChild(note);
+}
